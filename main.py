c86d29299b6db1d273de8a8b8d87dfd719e2fdde
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-01-12 22:57:42 -0300

    Rename codigo.py para main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.press("super")
+pyautogui.write("firefox")
+pyautogui.press("enter")
+
+
+# https://dlp.hashtagtreinamentos.com/python/intensivao/login
